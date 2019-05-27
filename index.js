f0a3fd2c62b2731a6e3503beb71d0adbae328cb6
blackstuend <lin70208@gmail.com>
2019-05-27 14:50:45 +0800

    modify some bug

--- index.js
+++ index.js
@@ -19,8 +19,14 @@ async function Auto_write(account, password, delay, option) { //option 0~5
     }, account, password)
     await page.waitFor(delay)
     await menu.evaluate(function () {  //模擬按下教學評量
-        let choose = document.querySelectorAll('.ob_td')[34].querySelector('div')
-        choose.click();
+        for(var i =20 ;i<document.querySelectorAll('.ob_td').length;i++){
+            console.log(i)
+            if(document.querySelectorAll('.ob_td')[i].innerHTML.includes('教學評量登')){
+                console.log(i)
+                let choose = document.querySelectorAll('.ob_td')[i].querySelector('div')
+                return choose.click();
+            }
+        }
     })
     await page.waitFor(delay)
     var class_length = await main.evaluate(function () { //取得有幾堂課 class_length -1 = 最多堂課 從1開始
@@ -31,11 +37,10 @@ async function Auto_write(account, password, delay, option) { //option 0~5
         dialog.dismiss();
     });
     for (i = 1; i < class_length - 1; i++) { //模擬選取每堂課 判斷是否完成 
-        check_filled = await main.evaluate(function (i) { 
-            var all_tr = document.querySelectorAll('table')[3].querySelectorAll('tr') 
+        check_filled = await main.evaluate(function (i) {
+            var all_tr = document.querySelectorAll('table')[3].querySelectorAll('tr')
             if (all_tr[i].querySelectorAll('td')[6].innerText == '未完成') { //如果未完成 就進行下面步驟 已完成就換下一個
                 all_tr[i].click();
-                console.log(i);
                 return true
             }
             else {
@@ -49,16 +54,18 @@ async function Auto_write(account, password, delay, option) { //option 0~5
             var table = document.querySelectorAll('table')[4];
             var all_tr = table.querySelectorAll('tr')
             for (let j = 3; j < all_tr.length - 1; j++) {
-                if (option == 'random')
-                    option = Math.floor(Math.random() * 5)
-                all_tr[j].querySelectorAll('input')[option].click()
+                if (all_tr[j].querySelectorAll('td')[0].innerHTML.includes('input')) {
+                    if (option == 'random')
+                        option = Math.floor(Math.random() * 5)
+                    all_tr[j].querySelectorAll('input')[option].click()
+                }
             }
             document.querySelector('center').querySelectorAll('input')[1].click()
         }, option)
         await page.waitFor(delay)
     }
     console.log('完成')
-    page.screentshot({path: 'finish.png'})
+    page.screentshot({ path: 'finish.png' })
     browser.close()
 }
 var account = process.argv[2]
